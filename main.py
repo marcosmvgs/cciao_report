d2eadf01af0b326089369cc7214ab03bb458f1e9
marcosmvgs <13080marcos.tupa@gmail.com>
2023-05-30 11:16:09 -0300

    carregando dados do sheets

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-import streamlit as st
+import pandas as pd
+
+
+def load_data():
+    sheet_id = "1RFmwDvSbIsRETxJ-yiaXJwHIa_psVugbPCC1tZwukTo"
+    df = pd.read_csv(f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv")
+    return df
+
+
+
+
 

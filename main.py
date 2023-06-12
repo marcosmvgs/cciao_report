9b075ea6ec931452b64c7c1d827f0aae083cf137
marcosmvgs <13080marcos.tupa@gmail.com>
2023-06-11 23:03:14 -0300

    Inserindo gráfico de horas por mês

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import datetime
-
-import numpy as np
+import re
+import altair as alt
+import altair
+import pandas as pd
 import streamlit as st
-
 import constants
 import funcs
 from PIL import Image
 import os
-
 
 st.set_page_config(layout='wide',
                    page_title='2º/6º GAV - Controle CCIAO',
@@ -39,15 +39,23 @@ esquadrao_database = raw_database.filter(items=['Matrícula da aeronave',
                                                 'Tipo de Registro',
                                                 'Tempo total de voo',
                                                 'Esforço Aéreo',
-                                                'Consumo Combustível (kg)'])
+                                                'Consumo Combustível (kg)',
+                                                'Dados - Decolagem'])
 esquadrao_database['Tempo total de voo'] = esquadrao_database['Tempo total de voo'].apply(lambda x: funcs.make_delta(x))
+data_pattern = re.compile('[0-9]{2}-[0-9]{2}-[0-9]{4}')
+esquadrao_database['Data - DEP'] = esquadrao_database['Dados - Decolagem'].apply(
+    lambda x: re.findall(data_pattern, x)[0])
+esquadrao_database['Data - DEP'] = pd.to_datetime(esquadrao_database['Data - DEP'])
+esquadrao_database = esquadrao_database.drop(columns='Dados - Decolagem')
 
 horas_voadas = esquadrao_database['Tempo total de voo'].sum()
 horas_disp = constants.HORAS_TOTAIS - horas_voadas
 perc_voado = round((horas_voadas / constants.HORAS_TOTAIS) * 100, 2)
-horas_voadas_e99 = esquadrao_database[esquadrao_database['Matrícula da aeronave'].str.contains('00|01|02|03|04|05')]['Tempo total de voo'].sum()
+horas_voadas_e99 = esquadrao_database[esquadrao_database['Matrícula da aeronave'].str.contains('00|01|02|03|04|05')][
+    'Tempo total de voo'].sum()
 horas_disp_e99 = constants.HORAS_TOTAIS_E99 - horas_voadas_e99
-horas_voadas_r99 = esquadrao_database[esquadrao_database['Matrícula da aeronave'].str.contains('50|52')]['Tempo total de voo'].sum()
+horas_voadas_r99 = esquadrao_database[esquadrao_database['Matrícula da aeronave'].str.contains('50|52')][
+    'Tempo total de voo'].sum()
 horas_disp_r99 = constants.HORAS_TOTAIS_R99 - horas_voadas_r99
 perc_voado_e99 = round((horas_voadas_e99 / constants.HORAS_TOTAIS_E99) * 100, 2)
 perc_voado_r99 = round((horas_voadas_r99 / constants.HORAS_TOTAIS_R99) * 100, 2)
@@ -70,6 +78,39 @@ with col4:
     kpi_perc_voado = st.metric(label='Percentual voado', value=f'{perc_voado}%')
     kpi_perc_voado_E99 = st.metric(label='Percentual voado_E99', value=f'{perc_voado_e99}%')
     kpi_perc_voado_R99 = st.metric(label='Percentual voado_R99', value=f'{perc_voado_r99}%')
+
+esquadrao_agrupado = esquadrao_database.groupby(
+    [pd.Grouper(key='Data - DEP', freq='M'), 'Matrícula da aeronave']).aggregate({'Tempo total de voo': 'sum'})
+esquadrao_agrupado2 = esquadrao_database.groupby(pd.Grouper(key='Data - DEP', freq='M')).aggregate(
+    {'Tempo total de voo': 'sum'})
+esquadrao_agrupado = esquadrao_agrupado.reset_index()
+esquadrao_chart_base = altair.Chart(esquadrao_agrupado)
+
+esquadrao_chart1 = esquadrao_chart_base.mark_area(line={'color': 'grey',
+                                                        'opacity': 0.3},
+                                                  opacity=0.5,
+                                                  point={'color': 'black',
+                                                         'size': 70},
+                                                  color=alt.Gradient(
+                                                      gradient='linear',
+                                                      stops=[alt.GradientStop(color='white', offset=0),
+                                                             alt.GradientStop(color='grey', offset=1)],
+                                                      x1=1,
+                                                      x2=1,
+                                                      y1=1,
+                                                      y2=0)).encode(
+    x=alt.X('Data - DEP:T', timeUnit='month', title=''),
+    y='sum(Tempo total de voo)',
+)
+
+esquadrao_chart2 = esquadrao_chart_base.mark_line().encode(
+    x=alt.X('Data - DEP:T', timeUnit='month', title=''),
+    y=alt.Y('Tempo total de voo:Q', title=''),
+    color=alt.Color('Matrícula da aeronave:N', legend=alt.Legend(orient='top')),
+)
+
+st.altair_chart((esquadrao_chart1 + esquadrao_chart2), use_container_width=True)
+
 st.markdown('---')
 
 st.markdown('### Tabela de voos :airplane:')
@@ -77,8 +118,8 @@ st.markdown('Cada linha representa o voo de um único tripulante.')
 st.markdown('Exemplo: um voo com 8 tripulantes será representado em 8 linhas. Esses voos serão'
             'representados pelo mesmo **Flight ID** ')
 mostrar_voos = st.checkbox('Mostrar voos selecionados')
+df_filtered = funcs.filter_dataframe(pilots_database)
 if mostrar_voos:
-    df_filtered = funcs.filter_dataframe(pilots_database)
     st.write(df_filtered)
 st.markdown('---')
 
